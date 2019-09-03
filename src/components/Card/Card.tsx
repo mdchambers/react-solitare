6f43b4b4594631c9ce544ef6cd7053cd12de3cd7
Mike Chambers <mchamber0@ucla.edu>
2019-09-02 20:19:11 -0400

    Implemented simple card render

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,42 @@
 import React from "react";
 
+import classes from "./Card.module.scss";
+
+// var context = require.context("../../assets/cards", true, /\.(svg)$/);
+// var files = {};
+
+// context.keys().forEach(filename => {
+//   files[filename] = context(filename);
+// });
+// console.log(files);
+
 interface Props {
-  firstname: string;
-  lastname: string;
+  suite: number;
+  value: number;
 }
 
+const cardDir: string = process.env.PUBLIC_URL + "/img/cards";
+const suiteMap: string[] = ["clubs", "diamonds", "hearts", "spades"];
+const valueMap: string[] = ["jack", "queen", "king"];
+
 const Card = (props: Props) => {
-  return <div></div>;
+  const fetchImgUrl = (suite: number, value: number) => {
+    let uri = cardDir + "/";
+    if (value > 9) {
+      uri += valueMap[value - 10];
+    } else {
+      uri += value;
+    }
+    uri += "_of_" + suiteMap[suite] + ".svg";
+    return uri;
+  };
+
+  const imgURI = fetchImgUrl(props.suite, props.value);
+  return (
+    <div className={classes.card}>
+      <img src={imgURI}></img>
+    </div>
+  );
 };
 
 export default Card;
--- src/containers/Board/Board.tsx
+++ src/containers/Board/Board.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+import Card from "../../components/Card/Card";
+
 import classes from "./Board.module.scss";
 
 const Board = () => {
-  return <div className={classes.board}></div>;
+  return (
+    <div className={classes.board}>
+      <Card suite={0} value={5} />
+      <Card suite={1} value={6} />
+    </div>
+  );
 };
 
 export default Board;
